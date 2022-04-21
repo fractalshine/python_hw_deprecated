164517bd48c3bbee398de3a674fab515f6c7b29b
fractalshine <96349158+fractalshine@users.noreply.github.com>
2022-04-21 17:49:50 +0300

    Update main.py

--- 7-hw/hw/main.py
+++ 7-hw/hw/main.py
@@ -1,15 +1,26 @@
 from utils import *
 
+
 pk_student_input = int(input("Введите номер студента\n"))
 
-student_dict = get_student_by_pk(pk_student_input)
+try:
+    student_dict = get_student_by_pk(pk_student_input)
+except IndexError:
+    print("Такого юзера нет")
+    quit(0)
+# student_dict = get_student_by_pk(pk_student_input)
 student = student_dict['full_name']
 student_skills = student_dict['skills']
+
 print(f'Студент {student}\n'
       f'Знает: {", ".join(student_skills)}')
 
 ask_profession = input(f"выберите специальность для оценки студента {student}\n").title()
 
-fitness_dict = check_fitness(pk_student_input, ask_profession)
+try:
+    fitness_dict = check_fitness(pk_student_input, ask_profession)
+except TypeError:
+    print("Такой профы нет")
+    quit(0)
 
 print(fitness_dict)
