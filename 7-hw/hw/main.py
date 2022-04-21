6a11ac3b0b02c08ebbe033f3f6e3100f7b86bb67
fractalshine <96349158+fractalshine@users.noreply.github.com>
2022-04-21 22:19:26 +0300

    Update main.py

--- 7-hw/hw/main.py
+++ 7-hw/hw/main.py
@@ -4,11 +4,12 @@ from utils import *
 pk_student_input = int(input("Введите номер студента\n"))
 
 try:
-    student_dict = get_student_by_pk(pk_student_input)
+    get_student_by_pk(pk_student_input)
 except IndexError:
-    print("Такого юзера нет")
+    print("Такого студента нет")
     quit(0)
-# student_dict = get_student_by_pk(pk_student_input)
+
+student_dict = get_student_by_pk(pk_student_input)
 student = student_dict['full_name']
 student_skills = student_dict['skills']
 
@@ -18,9 +19,18 @@ print(f'Студент {student}\n'
 ask_profession = input(f"выберите специальность для оценки студента {student}\n").title()
 
 try:
-    fitness_dict = check_fitness(pk_student_input, ask_profession)
+    check_fitness(pk_student_input, ask_profession)
 except TypeError:
     print("Такой профы нет")
     quit(0)
 
-print(fitness_dict)
+fitness_dict = check_fitness(pk_student_input, ask_profession)
+if len(fitness_dict['relevant']) != 0:
+    relevant_skills = ", ".join(fitness_dict['relevant'])
+else:
+    relevant_skills = "нет подходящих навыков"
+lacks_skills = ", ".join(fitness_dict['lacks'])
+fit_percent = fitness_dict['fit_percent']
+print(f'Пригодность {fit_percent}\n'
+      f'{student} знает: {relevant_skills}\n'
+      f'{student} не знает: {lacks_skills}')
